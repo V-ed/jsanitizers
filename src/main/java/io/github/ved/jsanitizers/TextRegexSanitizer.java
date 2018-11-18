e9cb36f307fb123c08d116a8a131901ed9e7e626
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-11-18 01:26:11 -0500

    Tweak TextRegexSanitizer to ease its usage
    - Removed shouldCheckPattern argument : it should always check for the pattern, not checking it creates a potential breach in the sanitize process. I mainly added that for optimization purposes in the past, but I now use the output of the Pattern.compile() to match the value directly, optimizing even more than before this method.
    - Tweaked how shouldBox argument works : instead of boxing the regex, it allows to "unbox" easily the regex to allow for anything before and/or after the content of the original regex, making a better use case for easiness purposes.

--- src/main/java/io/github/ved/jsanitizers/TextRegexSanitizer.java
+++ src/main/java/io/github/ved/jsanitizers/TextRegexSanitizer.java
@@ -24,29 +24,20 @@ public interface TextRegexSanitizer {
 	static String sanitizeValue(Object value, String regexToMatch,
 			boolean isInverted, boolean shouldBox) throws BadFormatException,
 			PatternSyntaxException{
-		return TextRegexSanitizer.sanitizeValue(value, regexToMatch,
-				isInverted, shouldBox, true);
-	}
-	
-	static String sanitizeValue(Object value, String regexToMatch,
-			boolean isInverted, boolean shouldBox, boolean shouldCheckPattern)
-			throws BadFormatException, PatternSyntaxException{
 		
 		String stringValue = TextSanitizer.sanitizeValue(value);
 		
 		if(regexToMatch != null){
 			
-			if(shouldCheckPattern){
-				// Test if Regex provided is valid
-				Pattern.compile(regexToMatch);
+			if(!shouldBox){
+				regexToMatch = ".*(" + regexToMatch + ").*";
 			}
 			
-			if(shouldBox && !regexToMatch.matches("^\\^.*\\$$")){
-				regexToMatch = "^" + regexToMatch + "$";
-			}
+			// Test if Regex provided is valid
+			Pattern pattern = Pattern.compile(regexToMatch);
 			
 			// Test regex and invert if we need to
-			if(stringValue.matches(regexToMatch) == isInverted){
+			if(pattern.matcher(stringValue).matches() == isInverted){
 				throw new BadFormatException(
 						"Value does not match the required pattern!",
 						FORMAT_NOT_MATCHING_PATTERN);
