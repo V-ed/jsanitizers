731088bc48558262fdec1e1c7100e663196f9f74
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2018-11-19 19:15:21 -0500

    Tweak BooleanSanitizer to handle empty values

--- src/main/java/io/github/ved/jsanitizers/BooleanSanitizer.java
+++ src/main/java/io/github/ved/jsanitizers/BooleanSanitizer.java
@@ -4,13 +4,14 @@ import io.github.ved.jsanitizers.exceptions.BadFormatException;
 
 public interface BooleanSanitizer {
 	
-	int FORMAT_NOT_BOOLEAN = 1;
+	int FORMAT_EMPTY = TextNotEmptySanitizer.FORMAT_EMPTY;
+	int FORMAT_NOT_BOOLEAN = 2;
 	
 	static boolean sanitizeValue(Object value) throws BadFormatException{
 		
-		String stringValue = TextSanitizer.sanitizeValue(value);
+		String stringValue = TextNotEmptySanitizer.sanitizeValue(value);
 		
-		if(!stringValue.matches("^(?i)true|false$")){
+		if(!stringValue.matches("(?i)^(true|false)$")){
 			throw new BadFormatException(
 					"Value cannot be something else than \"true\" or \"false\"!",
 					FORMAT_NOT_BOOLEAN);
